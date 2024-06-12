a54bd9ca61c949ac178d56b0534637622217046d
Andrei Luca <1881266+iamandrewluca@users.noreply.github.com>
2024-06-12 22:51:02 +0300

    chore: rename app

--- scripts/clone-1loc.sh
+++ scripts/clone-1loc.sh
@@ -1,7 +1,7 @@
 rm -rf 1loc
-rm -rf src/content/lines
+rm -rf src/content/lines/*
 mkdir -p src/content/lines
 git clone --depth 1 https://github.com/iamandrewluca/1loc.git 1loc
-cp -r 1loc/contents/ src/content/lines/
+cp -r 1loc/contents/* src/content/lines/
 rm -rf 1loc
 ls -l src/content/lines
